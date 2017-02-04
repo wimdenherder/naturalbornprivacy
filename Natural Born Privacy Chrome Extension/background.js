6db996715e2cc19e4db8abe8036c3d191be98cb2
profikid <profikid@gmail.com>
2017-02-04 17:06:13 +0100

    fix: code cleanup and loading image as base64 to work for sites that have strict policies around https for example github.com

--- Natural Born Privacy Chrome Extension/background.js
+++ Natural Born Privacy Chrome Extension/background.js
@@ -2,9 +2,25 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+console.log('test');
+
 chrome.browserAction.onClicked.addListener(function(tab) {
   console.log('Put on your red glasses to read ' + tab.url + '!');
   chrome.tabs.executeScript({
     file: 'effect.js'
   });
 });
+
+//returns boolean
+function toggleEffect(){
+    chrome.storage.sync.set({'active': true}, function() {
+          // Notify that we saved.
+          message('Settings saved');
+        });
+}
+
+function get(){
+        chrome.storage.sync.get('active', function(test){
+            console.log('active', test);
+        });
+    }
