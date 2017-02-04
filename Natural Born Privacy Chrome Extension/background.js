2effa691ed6ef970da08d7fdb63a15a9b132f7f3
profikid <profikid@gmail.com>
2017-02-04 19:29:11 +0100

    refactor: add some structure and todo

--- Natural Born Privacy Chrome Extension/background.js
+++ Natural Born Privacy Chrome Extension/background.js
@@ -2,13 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-console.log('plugin loaded');
-
-console.log('toggle',toggleEffect());
-console.log('toggleGet',get());
-
-let isActive = false;
-
 chrome.browserAction.onClicked.addListener(function(tab) {
   console.log('Put on your red glasses to read ' + tab.url + '!');
 
@@ -16,17 +9,3 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     file: 'effect.js'
   });
 });
-
-//returns boolean
-function toggleEffect(){
-    chrome.storage.sync.set({'active': true}, function() {
-        // Notify that we saved.
-        console.log('message saved');
-    });
-}
-
-function get(){
-    chrome.storage.sync.get('active', function(state){
-        isActive = state;
-    });
-}
---
+++ TODO.md
@@ -0,0 +1,5 @@
+*   setup structure
+*   fix manifest and accessing images and styles from external sources
+*   on off toggle with icon
+*   popup menu with different styles
+*   refactor styles to CSS so when clicked item on popup is clicked a style This way new elements will be applied to the special style
