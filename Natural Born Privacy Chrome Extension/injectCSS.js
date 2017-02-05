3df35bb9f02e0149798fd7cc94aa95e829ec326e
profikid <profikid@gmail.com>
2017-02-05 22:22:33 +0100

    feat: toggle for styles

--- Natural Born Privacy Chrome Extension/injectCSS.js
+++ Natural Born Privacy Chrome Extension/injectCSS.js
@@ -1,27 +1,32 @@
 console.clear();
-console.log('injectCSS.js loaded');
 
+var themesUrl = chrome.extension.getURL('styles.json');
 var cssId = 'naturalBornPrivacy'; 
-var theme = {
-        title:'Pink glasses',
-        iconImage:'pink.png',
-        source:'pink.css'
+var themes = [];
+var index = 0; // first theme in styles.json
+
+//TODO set index from a popup menu
+
+//fetched json file and select index 0 to inject change index for different themes
+fetch(themesUrl)
+    .then(x=> x.json())
+    .then(x=> inject(x[index]));
+
+function inject(theme){
+    if (!document.getElementById(cssId))
+    {
+        var head  = document.getElementsByTagName('head')[0];
+        var link  = document.createElement('link');
+        link.id   = cssId;
+        link.rel  = 'stylesheet';
+        link.type = 'text/css';
+        link.href = chrome.extension.getURL(`styles/${theme.source}`);
+        link.media = 'all';
+
+        head.appendChild(link);
+    }else{
+        //when active and clicked style will be removed
+        document.getElementById(cssId).remove();
     }
 
-if (!document.getElementById(cssId))
-{
-    var head  = document.getElementsByTagName('head')[0];
-    var link  = document.createElement('link');
-    link.id   = cssId;
-    link.rel  = 'stylesheet';
-    link.type = 'text/css';
-    link.href = chrome.extension.getURL(`styles/${theme.source}`);
-    link.media = 'all';
-
-    console.log(link);
-
-    head.appendChild(link);
-}else if(theme.source === "remove"){
-    document.getElementById(cssId).remove();
 }
-
