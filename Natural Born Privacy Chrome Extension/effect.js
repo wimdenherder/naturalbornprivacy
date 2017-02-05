33a32bede8ca6b48ba3efd97956c179f2ee6ca38
profikid <profikid@gmail.com>
2017-02-05 20:45:00 +0100

    refactor: add structure and blending modes for images and video's

--- Natural Born Privacy Chrome Extension/effect.js
+++ Natural Born Privacy Chrome Extension/effect.js
@@ -1,6 +1,11 @@
-var bgImage = chrome.extension.getURL("secret-background2.png");
+var bgImage = chrome.extension.getURL("images/secret-background2.png");
 
+var imgs = document.querySelectorAll("img");
+var vids = document.querySelectorAll("video");
 var divs = document.querySelectorAll('body,input,textarea,code,pre,li,strong,iframe,div,p,td,th,em,a,button,html,article,header,span,blockquote,quote,section,h1,h2,h3,h4,h5,button');
+
+// een event?
+
 
 divs.forEach(d=>{
     // staat mooier in aparte css bestanden ipv javascript. beter uitbreidbaar
@@ -10,11 +15,6 @@ divs.forEach(d=>{
     d.style.fontSize = "12px"; 
 });
 
-var imgs = document.querySelectorAll("img");
-imgs.forEach(img =>{
-    // plak een ander divje met de juiste kleur precies over de plaatjes
-    // gebruik blending modes om plaatje te kleuren
-});
-
-var vids = document.querySelectorAll("video");
-vids.forEach(vid => console.log(vid.src));
+//https://css-tricks.com/almanac/properties/m/mix-blend-mode/
+imgs.forEach(img => img.style.mixBlendMode = "multiply");
+vids.forEach(vid => vid.style.mixBlendMode = "multiply");
